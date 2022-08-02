34f5818f3c210bb2c3cde0a4b985bcecf621434f
Julio Bissoli <juliobissoli33@gmail.com>
2022-08-01 22:07:02 -0300

    Address in property

--- src/properties/dto/create-property.dto.ts
+++ src/properties/dto/create-property.dto.ts
@@ -1,6 +1,12 @@
 export class CreatePropertyDto {
     status: string;
     label: string;
-    user_id: string;
     images: string[];
+    user_id: string; // remover depois fazer com o auth no service
+    address: {
+        label: string;
+        cep: string;
+        city: string;
+        public_place: string;
+    }
 }
